c54d16b23298d11b4cd5909a366ea760e99f60ac
ShantMelikyan <shantmelikyan@gmail.com>
2020-02-08 22:39:00 -0800

    command line input working, need to change fope,to open

--- demo.c
+++ demo.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
 
-int main(int argc, char** argv){
+void readFromfile(FILE *fptr);
+void readFromCMD(int argc, char **argv);
+
+int main(int argc, char** argv)
+{
 	int result = 0;
-	char binary[100];
-	char str2[8];
-
+	int fp_to_read = 0;
 	printf("you have entered: %d values\n", argc); 
-	
 	printf("Values entered: ");
 
 	for (int i = 0; i < argc; ++i) 
@@ -16,53 +21,108 @@ int main(int argc, char** argv){
     
 //===================== determine if reading from cmd or file ============================
     result = strcmp(argv[1], "-");
-  		if (result == 0)
+	printf("Original ASCII    Decimal  Parity\n");
+	printf("-------- -------- -------- -------- \n");
+  		if(argc > 2) // reading from cmd
+		  {
    			printf("Reading from commandline! \n");
-   		else
-   			printf("Reading from file!\n");
-	
-	
-//====================== open file =====================================
-   FILE *fptr;
-   if ((fptr = fopen("zero_and_one.txt","r")) == NULL){
-       printf("Error! opening file");
-       
-       exit(1);
-   }
-//====================== read from file =================================
-   printf("Original ASCII    Decimal  Parity\n");
-   while (fgets(binary, sizeof(binary), fptr) != NULL) 
-   {
+			readFromCMD(argc, argv);
+		  }
+   		else if(argc == 2) // reading from file
+   		{
+			printf("Reading from file!\n");
+			FILE *fptr;
+   				if ((fptr = fopen("zero_and_one.txt","r")) == NULL)
+				   {
+   						printf("Error! opening file");
+						exit(1);
+   					}
+			
+			readFromfile(fptr);
+		}
+		else
+			printf("cant open file");
+			   
+   
+return 0;
+
+}
+
+void readFromfile(FILE *fptr)
+{ 
+	char str2[8];
+	char binary[100];
+	while (fgets(binary, sizeof(binary), fptr) != NULL) 
+	{
    	
    		int num = 0;
    		int sum = 0;
    		char* ptr;
-   		int decimal = 0;
-
-  
+   		int size = strlen(binary);
+		if(size < 9)
+		{
+			for(int j = size; j < 9; j++)
+            {
+            	binary[j] = '0';
+            }
+            binary[8] = '\0';
+   		}
         for(int i = 0; i < 8; i++)
    		{	
    			str2[i] = binary[i+1];
    			sum +=(int)binary[i];
    		}
       	char ascii = strtol(str2, &ptr, 2);
-      	decimal = (int)ascii;
-        //for(int i = 0; i < 8; i++)
-			//num += (binary[i] - '0') << (7-i);
+      	int	decimal = (int)ascii;
 		
-
-		//fprintf(stderr, "%s ", binary);
 		for (int i = 0; i < 8; i++)
 			printf("%c", binary[i]);
-		printf("   %c       %d  \n", ascii, decimal);
 
+		printf("\t%c\t%d\t", ascii, decimal );
+			if(sum % 2 == 0)
+				printf("EVEN\n");
+			else 
+				printf("ODD\n");
 
    }
-        
    fclose(fptr); 
+}
+void readFromCMD(int argc, char **argv)
+{
+	char binary[9];
+    char str2[8];
 
+    for(int i = 2; i < argc; i++)
+    {
+		int sum = 0;
+		char *ptr;
 
+        strcpy(binary, argv[i]);
+        int size = strlen(argv[i]);
 
-	return 0;
+        if(size < 9)
+        {
+            for(int j = size; j < 9; j++)
+            {
+            	binary[j] = '0';
+            }
+            binary[8] = '\0';
+        }
+        for(int j = 0; j < 8; j++)
+        {
+            str2[j] = binary[j + 1];
+            sum += (int)binary[j];
+		}
+                        
+        
+        char ascii = strtol(str2, &ptr, 2);
+        int decimal = (int)ascii; 
 
+		printf("%s\t%c\t%i\t", binary, ascii, decimal);
+		if(sum % 2 == 0)
+			printf("EVEN\n");
+		else 
+			printf("ODD\n");
+                    
+    }
 }
