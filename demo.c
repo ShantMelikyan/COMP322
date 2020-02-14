df90a5ee916a81d8667c1f036a9d20bad4b423f1
ShantMelikyan <shantmelikyan@gmail.com>
2020-02-14 12:39:33 -0800

    changed two function to just one for both stdin and file input

--- demo.c
+++ demo.c
@@ -6,153 +6,111 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-void readFromfile(char fptr[]);
-void readFromCMD(int argc, char **argv);
-int valid_output(int ascii);
+void calculations(char fptr[]);
 
-const char *values[] = { "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
+const char *VALUES[] = { "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
 						"BS", "HT", "LF", "VT", "FF", "CR", "SO", "SI", "DLE",
 						"DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN",
 						"EM", "SUB", "ESC", "FS", "GS", "RS", "US", "SPACE" };
+const char*DEL = "DEL";
 
 int main(int argc, char** argv)
 {
-	
-	char c;
 
-  	if(argc > 2) // reading from cmd
+	char c;
+	int pos = 0;
+	char line[9];
+	int start_pos = 1;
+	char binary[9];
+
+  	if(argc >= 2 && (open(argv[1], O_RDONLY)) == -1) // reading from stdin
 	{
-   		printf("Reading from commandline! \n");
-		readFromCMD(argc, argv);
+		printf("\ncmd\n");
+		if(argv[1][0] == '-')
+			start_pos = 2;
+		printf("\nOriginal ASCII    Decimal  Parity\n");
+		printf(  "-------- -------- -------- -------- \n");
+		for(int i = start_pos ; i < argc; i++)
+		{
+			strcpy(binary, argv[i]);
+			calculations( binary);
+			
+		}
+
 	}
-   	else if(argc == 2 && argv[1][0] != '-') // reading from file
+   	else if(argc == 2 && argv[1][0] != '-' && (open(argv[1], O_RDONLY) != -1)) // reading from file
    	{
-		printf("Reading from file!\n");
 		
 		int filedesc = open(argv[1], O_RDONLY);
-		if(filedesc)
-			printf("\nfile desc = %d\n", filedesc);
-		else
-			printf("something wrong");
 		
-		int pos = 0;
-		char line[9];
-			printf("\nOriginal ASCII    Decimal  Parity\n");
-			printf("-------- -------- -------- -------- \n");
-			while(read(filedesc, &c, 1) != 0 )
-			{
-				if(c == '\n'){
 
-					line[pos] = '\0';
-					//printf("%s\n", line);
-					readFromfile(line);
-					pos = 0;
-				}
-				else{
-					line[pos] = c;
-					//printf("%c", c);
-					pos++;
-				}
+		printf("\nOriginal   ASCII    Decimal  Parity\n");
+		printf("-------- -------- -------- -------- \n");
+
+		while(read(filedesc, &c, 1) != 0 )	// iterating through the file 
+		{
+			if(c == '\n' || c == ' ')		// if newline or space detected 
+			{
+
+				line[pos] = '\0'; 			// end the binary(ascii) number 
+				calculations( line);		// pass the line for further calculations
+				pos = 0;
+			}
+			else  							// continue adding chars to the binary(ascii)
+			{
+				line[pos] = c;
+				pos++;
 			}
 
-	
-		
+		}
 
 	}
 	else 
 		printf("Wrong input");
 			   
-   
 	return 0;
 
 }
 
-void readFromfile(char* fptr)
+void calculations(char* fptr)
 { 
 	char str2[8];
-	char binary[100];
-	
-
-		strcpy(binary, fptr);		
-
-   		int sum = 0;
-   		char* ptr;
-   		int size = strlen(binary);
-		if(size < 9)
-		{
-			for(int j = size; j < 9; j++)
-            {
-            	binary[j] = '0';
-            }
-            binary[8] = '\0';
-   		}
-        for(int i = 0; i < 8; i++)
-   		{	
-   			str2[i] = binary[i+1];
-   			sum +=(int)binary[i];
-   		}
-      	char ascii = strtol(str2, &ptr, 2);
-      	int	decimal = (int)ascii;
-		
-			//for (int i = 0; i < 8; i++)
-			//	printf("%c", binary[i]);
-
-			if((ascii >= 0 && ascii <= 32) || ascii == 127)
-				printf("%s\t%s\t%i\t",binary, values[decimal], decimal);
-			else
-				printf("%s\t%c\t%i\t",binary, ascii, decimal);
-
-			if(sum % 2 == 0)
-				printf("EVEN\n");
-			else 
-				printf("ODD\n");
-		     
-
-
-}
-
-void readFromCMD(int argc, char **argv)
-{
 	char binary[9];
-    char str2[8];
-		printf("\nOriginal ASCII    Decimal  Parity\n");
-		printf("-------- -------- -------- -------- \n");
-    for(int i = 2; i < argc; i++)
-    {
-		int sum = 0;
-		char *ptr;
 
-        strcpy(binary, argv[i]);
-        int size = strlen(argv[i]);
+	strcpy(binary, fptr);		
 
-        if(size < 9)
+   	int sum = 0;
+   	char* ptr;
+   	int size = strlen(binary);
+
+	if(size < 9)
+	{
+		for(int j = size; j < 9; j++)
         {
-            for(int j = size; j < 9; j++)
-            {
-            	binary[j] = '0';
-            }
-            binary[8] = '\0';
+         	binary[j] = '0';
         }
-        for(int j = 0; j < 8; j++)
-        {
-            str2[j] = binary[j + 1];
-            sum += (int)binary[j];
-		}
-                        
-        
-        char ascii = strtol(str2, &ptr, 2);
-        int decimal = (int)ascii; 
 
-			if((ascii >= 0 && ascii <= 32) || ascii == 127)
-				printf("%s\t%s\t%i\t", binary, values[decimal], decimal);
-			else
-				printf("%s\t%c\t%i\t", binary, ascii, decimal);
-			if(sum % 2 == 0)
-				printf("EVEN\n");
-			else 
-				printf("ODD\n");
-	
-                    
-    }
+        binary[8] = '\0';
+   	}
+    for(int i = 0; i < 8; i++)
+   	{	
+   		str2[i] = binary[i+1];
+   		sum +=(int)binary[i];
+   	}
 
+    char ascii = strtol(str2, &ptr, 2);
+    int	decimal = (int)ascii;
+		
+	if((ascii >= 0 && ascii <= 32))
+		printf("%s\t%s\t%i\t",binary, VALUES[decimal], decimal);
+	else if(ascii == 127)
+		printf("%s\t%s\t%i\t",binary, DEL, decimal);
+	else
+		printf("%s\t%c\t%i\t",binary, ascii, decimal);
+
+	if(sum % 2 == 0)
+		printf("EVEN\n");
+	else 
+		printf("ODD\n");
+		     
 }
