bfeb94c9c3462140ab580e4ee81d0bcb6188f767
ShantMElikyan <shantmelikyan@gmail.com>
2020-02-29 00:23:10 -0800

    program seems to work, but have 0 for system time

--- lab1/time-4-baby-and-me.c
+++ lab1/time-4-baby-and-me.c
@@ -1,44 +1,69 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/times.h>
+#include <inttypes.h>
 
 void report(pid_t child, int status);
 
 int main(/*int agrc, char** agrv*/)
 {
-    printf("hello world");
-    time_t start, stop; 
+
     pid_t pid;
     int status;
-
-    start = time(NULL); 
+    
+    time_t start;
+    time(&start);
     printf("\nSTART: %ld\n", start);
-    pid = fork(); // create pid process
 
+    sleep(1); // sleep for 1 sec 
+    for(int i = 0, k = 0; i < 99999999; i++) // making cpu busy
+        k=k*i;
+   
+    pid = fork(); // create pid process
+    struct tms st_cpu;  
+    times(&st_cpu);  
+
+    long k = 0;
+
+    for(int i = 0, k = 0; i < 99999999; i++)
+        k=k*i;
+    sleep(2);
+    
     if (pid == -1) {               // checking if the fork was made 
         printf("cant create a fork :( ");
         exit(0);
     }
-
+   
 
     if (pid == 0){    
-   
+        
         report(-1, -1);   
-        return 0;
+        exit(EXIT_SUCCESS);
     }
     else{
+        
         pid_t child;
-        child = waitpid(pid, &status, 0); // this way, the parent waits for all the pid processes 
+        child = waitpid(pid, &status, WUNTRACED); // the parent waits for all the child processes 
+        times(&st_cpu);
+
         report(child, status);   
     }
+   
     
-    stop = time(NULL); 
-    printf("\nSTOP: %ld\n", stop);
-    
+
+    printf("USER: %d, SYS: %d\nCUSER: %d, CSYS: %d\n",  (int)(st_cpu.tms_utime ),
+                                                        (int)(st_cpu.tms_stime ), 
+                                                        (int)(st_cpu.tms_cutime ),  
+                                                        (int)(st_cpu.tms_cstime ) );
+    time(&start);
+    printf("\nSTOP: %ld\n", start);
+
+    printf("%ld", k);
     return 0;
 }
 
@@ -49,6 +74,5 @@ void report(pid_t child, int status)
         printf("\nPPID: %d, PID: %d" , (int) getppid(), (int) getpid() );
     else
         printf("\nPPID: %d, PID: %d, CPID: %d, RETVAL: %d\n", (int) getppid(), (int) getpid(), child, status);
-
 
 }
