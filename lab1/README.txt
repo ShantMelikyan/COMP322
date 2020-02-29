3c0ef5867a8a5788473a310629ca94884c7083cc
ShantMElikyan <shantmelikyan@gmail.com>
2020-02-29 10:40:32 -0800

    final changes. project finished

--- lab1/README.txt
+++ lab1/README.txt
@@ -1,19 +1,36 @@
-Lab1
-2/28/2020
+https://github.com/ShantMelikyan/COMP322/tree/master/lab1
+shant.melikyan.17@my.csun.edu 
+Student ID: 200915110
 
+=================================README======================================
+
+Lab1 - Time and Forking Children
+
+Project Status: the program is working properly
+
+2/29/2020
+1) clarifications concerning the 0 in the output
+2) project finished
+
+2/28/2020
 1) minor changes, changed start variable from clock_t to time_t
 2) push to github
 
 2/22/2020
-
 1) fixed the output for parent and child report
 2) time is always 0, added delays to check if its working 
 3) user times can be impacted but for system time its always 0
 
 
 2/21/2020
-
 1) reading book chapter to understand the concept
 2) reading man pages on time(), fork(), waitpid() and testing everyting 
 3) something is wrong, child report is printing first
 4) commit to github. push
+
+=============================================================================
+
+
+
+
+
--- lab1/time-4-baby-and-me.c
+++ lab1/time-4-baby-and-me.c
@@ -6,11 +6,10 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/times.h>
-#include <inttypes.h>
 
 void report(pid_t child, int status);
 
-int main(/*int agrc, char** agrv*/)
+int main()
 {
 
     pid_t pid;
@@ -20,51 +19,35 @@ int main(/*int agrc, char** agrv*/)
     time(&start);
     printf("\nSTART: %ld\n", start);
 
-    sleep(1); // sleep for 1 sec 
-    for(int i = 0, k = 0; i < 99999999; i++) // making cpu busy
-        k=k*i;
-   
-    pid = fork(); // create pid process
-    struct tms st_cpu;  
+    pid = fork(); // forking programm and creating pid process
+    struct tms st_cpu;
     times(&st_cpu);  
 
     long k = 0;
 
-    for(int i = 0, k = 0; i < 99999999; i++)
-        k=k*i;
-    sleep(2);
-    
     if (pid == -1) {               // checking if the fork was made 
         printf("cant create a fork :( ");
         exit(0);
     }
-   
-
     if (pid == 0){    
-        
         report(-1, -1);   
         exit(EXIT_SUCCESS);
     }
     else{
-        
         pid_t child;
         child = waitpid(pid, &status, WUNTRACED); // the parent waits for all the child processes 
         times(&st_cpu);
-
+        sleep(1);
         report(child, status);   
     }
    
-    
-
-    printf("USER: %d, SYS: %d\nCUSER: %d, CSYS: %d\n",  (int)(st_cpu.tms_utime ),
-                                                        (int)(st_cpu.tms_stime ), 
-                                                        (int)(st_cpu.tms_cutime ),  
-                                                        (int)(st_cpu.tms_cstime ) );
+    printf("USER: %d, SYS: %d\nCUSER: %d, CSYS: %d\n",  (int)(st_cpu.tms_utime ), (int)(st_cpu.tms_stime ), (int)(st_cpu.tms_cutime ), (int)(st_cpu.tms_cstime ) );
     time(&start);
     printf("\nSTOP: %ld\n", start);
 
     printf("%ld", k);
     return 0;
+
 }
 
 void report(pid_t child, int status)
