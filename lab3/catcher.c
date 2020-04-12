c24692ac9e945c13806030f87268dd99b8ae8095
shantmelikyan <shantmelikyan@gmail.com>
2020-04-11 20:08:12 -0700

    updates

--- lab3/catcher.c
+++ lab3/catcher.c
@@ -6,7 +6,8 @@
 #include <signal.h>
 #include <time.h>
 #include <string.h>
-//#include <sys/times.h>
+#include <stdlib.h>
+
 
 void handle_reg(int mysignal);
 
@@ -61,16 +62,22 @@ void handle_reg(int mysignal)
 
 }
 
+int sig_number()
+{
+
+}
+
 //+ The program processes the command line arguments
     //+ The arguments indicate which signals to catch
 //+ The program emits a status line that includes its PID to stderr 
 // The program registers a handler for each argument		              (see signal(2))
-// The program pauses itself continually 				  (see pause(2))
+//+ The program pauses itself continually 				  (see pause(2))
 // The handler registers itself again			(read about unreliable signals)
 // The handler emits a line to stdout that indicates 
 //      the signal caught, and 
-//      the time it was caught 					    (see time(2))
-// The program gracefully terminates after 
-//      receiving three successive SIGTERM signals	   (hint: static int count)
-// The program emits a final status message to stderr that indicates
-//      the number of signals caught
+//+      the time it was caught 					    (see time(2))
+//+ The program gracefully terminates after 
+//+      receiving three successive SIGTERM signals	   (hint: static int count)
+//+ The program emits a final status message to stderr that indicates
+//+     the number of signals caught
+
