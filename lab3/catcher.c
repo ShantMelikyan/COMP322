6c03cffc9b8219652d323cac1c774991cadc1df4
shantmelikyan <shantmelikyan@gmail.com>
2020-04-11 22:22:07 -0700

    signal catcher: working but >/dev/null outputs

--- lab3/catcher.c
+++ lab3/catcher.c
@@ -8,12 +8,12 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 void handle_reg(int mysignal);
+int sig_number(char*sig);
 
 static const char* signals[27] = {  "HUP", "INT", "QUIT", "ILL", "TRAP", "ABRT", "IOT", "BUS", "FPE","USR1", 
                                     "SEGV", "USR2", "PIPE", "ALRM", "TERM", "STKFLT", "CHLD", "CONT", "TSTP", 
-                                    "TTIN", "TTOU", "URG", "XCPU", "XFSZ", "VTALRM", "PROF", "WINCH" };
+                                    "TTIN", "TTOU", "URG", "XCPU", "XFSZ", "VTALRM", "PROF", "WINCH"};
 
 
 int main(int argc, char** argv)
@@ -24,18 +24,13 @@ int main(int argc, char** argv)
     }
     fprintf(stderr, "%s: $$ = %d\n", argv[0], getpid());
 
-    int i = 0;
-    for(int i = 1; i < argc; i++)
-    {
-        signal(SIGTERM, handle_reg);
+    for(int i = 1; i < argc; i++){
+        if(signal(sig_number(argv[i]), handle_reg) == SIG_ERR )
+            fprintf(stderr, "Can't handle signal");
     }
 
-    while(1)
-    {
-        printf("i = %d\n" , i);
-        i++;
-        sleep(1);
-        //pause();
+    while(1){
+        pause();
     }
 
     return 0;
@@ -43,41 +38,36 @@ int main(int argc, char** argv)
 
 void handle_reg(int mysignal)
 {
+
+    signal(mysignal, handle_reg);
     static int count_TERMs, total_sigs = 0;
     time_t start_time;
-	time(&start_time);
+    time(&start_time);
 
-    printf("SIG%d caught at %d\n", mysignal, (int)start_time);
+    fprintf(stderr,"SIG%s caught at %d\n", signals[mysignal-1], (int)start_time);
     total_sigs++;
+    
     if(mysignal == SIGTERM){
-		count_TERMs++;
+        count_TERMs++;
 	}
-	else {
-		count_TERMs = 0;
+	else{
+        count_TERMs = 0;
 	}
 
     if(count_TERMs == 3){
-        fprintf(stderr, "catcher: Total signals count = %d" , total_sigs);
+        fprintf(stderr, "catcher: Total signals count = %d\n" , total_sigs);
+        exit(EXIT_SUCCESS);
     }
 
 }
 
-int sig_number()
+int sig_number(char*sig)
 {
-
+    int ret_signal = -1;
+    for(unsigned int i = 0; i < 27; i++){
+        if(strcmp(sig, signals[i]) == 0){
+            ret_signal = i;
+        }
+    }
+    return ++ret_signal;
 }
-
-//+ The program processes the command line arguments
-    //+ The arguments indicate which signals to catch
-//+ The program emits a status line that includes its PID to stderr 
-// The program registers a handler for each argument		              (see signal(2))
-//+ The program pauses itself continually 				  (see pause(2))
-// The handler registers itself again			(read about unreliable signals)
-// The handler emits a line to stdout that indicates 
-//      the signal caught, and 
-//+      the time it was caught 					    (see time(2))
-//+ The program gracefully terminates after 
-//+      receiving three successive SIGTERM signals	   (hint: static int count)
-//+ The program emits a final status message to stderr that indicates
-//+     the number of signals caught
-
