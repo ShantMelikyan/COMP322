a1caf775f96574e6a77b14de05bb54b0457ceb7f
shantmelikyan <shantmelikyan@gmail.com>
2020-05-03 15:24:44 -0700

    finall push. project done

--- lab4/dining-p.c
+++ lab4/dining-p.c
@@ -13,12 +13,10 @@
 #include <pthread.h>
 #include <errno.h>
 
-#define CHOPSTICK1  "/chopstick_1"
-#define CHOPSTICK2  "/chopstick_2"
 static int exit_flag = 0;
 
-void eat(int phil_po);
-void think(int phil_po);
+void eat();
+void think();
 void signal_handler(int signal);
 
 struct Philosopher{
@@ -26,12 +24,14 @@ struct Philosopher{
     int position;
     int cycles;
     sem_t* chopsticks[2];
+    sem_t* returnVal;
+    char buff1[32];
+    char buff2[32];
     
 }phil;
 
 int main(int argc, char **argv)
 {
-    //struct Philosopher phil;
     
     if(argc != 3){
 		printf("Arguments required: 2, Arguments provided:%d \n", argc);
@@ -42,54 +42,80 @@ int main(int argc, char **argv)
 	phil.position = atoi(argv[2]);
     phil.cycles = 0;
 
-    if(phil.position > phil.seats) {
-        fprintf(stderr, "Seats are not avaliable\n");
+    if(phil.position > phil.seats){
+        fprintf(stderr, "Seats are not avaliable for philosopher: %d \n", phil.position);
         return 0;
     }
+    /* create name buffers for semaphores */ 
+    char chopst_name1[32];
+	char chopst_name2[32];
+	sprintf(chopst_name1, "%d", phil.position);
+	sprintf(chopst_name2, "%d", (phil.position+1) % phil.seats);
 
-	phil.chopsticks[0] = sem_open(CHOPSTICK1, O_CREAT, 0666, 1);
-    if(phil.chopsticks[0]==SEM_FAILED)
-		perror("cant creat semaphore 0");
-    phil.chopsticks[1] = sem_open(CHOPSTICK2, O_CREAT, 0666, 1);
-    if(phil.chopsticks[0]==SEM_FAILED)
-		perror("cant creat semaphore 0");
+    phil.buff1[0] = '/';  
+    strcat(phil.buff1, chopst_name1);
+	phil.buff2[0] = '/';  
+    strcat(phil.buff2, chopst_name2);
 
+    /* create semaphores */  
+	phil.returnVal = sem_open(phil.buff1, O_CREAT, 0666, 1);
+    if(phil.returnVal == SEM_FAILED)
+		perror("cant create semaphore 0");
+    phil.chopsticks[0] = phil.returnVal;
+
+    phil.returnVal = sem_open(phil.buff2, O_CREAT, 0666, 1);
+    if(phil.returnVal == SEM_FAILED)
+		perror("cant create semaphore 1");
+    phil.chopsticks[1] = phil.returnVal;
+
+    /* create signal handler */
     if(signal(SIGTERM, signal_handler) == SIG_ERR )
         fprintf(stderr, "Can't handle signal");
 
+    int sval0, sval1;
+    /* main logic algothithm */
     do{
-        sem_wait(phil.chopsticks[0]);
-        sem_wait(phil.chopsticks[1]);
-     
-        eat(phil.position);
-        
-        sem_post(phil.chopsticks[0]);
-        sem_post(phil.chopsticks[1]);
- 
-        think(phil.position);
-        phil.cycles = phil.cycles + 1;
-        
+        sem_getvalue(phil.chopsticks[0], &sval0);
+		sem_getvalue(phil.chopsticks[1], &sval1);
+
+        if(sval0 != 0 || sval1 != 0){
+            sem_wait(phil.chopsticks[0]);
+            sem_wait(phil.chopsticks[1]);
+            eat(phil.position);
+            
+            sem_post(phil.chopsticks[0]);
+            sem_post(phil.chopsticks[1]);
+            think(phil.position);
+            phil.cycles = phil.cycles + 1;
+        }
+
     } while (exit_flag == 0);
 
 }
 
-void eat(int phil_pos)
+void eat()
 {
-    printf("Philosopher %d is eating.\n", phil_pos);
+    printf("Philosopher %d is eating.\n", phil.position);
     usleep(rand() % 9999999);
 }
-void think(int phil_pos)
+
+void think()
 {
-    printf("Philosopher %d is thinking\n", phil_pos);
+    printf("Philosopher %d is thinking\n", phil.position);
     usleep(rand() % 9999999);
 }
 
 void signal_handler(int sig)
 {
     signal(SIGTERM, signal_handler);
-    if (sig == SIGTERM) 
+    if (sig == SIGTERM){
+        sem_close(phil.chopsticks[0]);
+		sem_close(phil.chopsticks[1]);
+		sem_unlink(phil.buff1);
+		sem_unlink(phil.buff2);
         exit_flag = 1;
-
+    }
+        
     fprintf(stderr, "Philosopher [%d] completed %d cycles\n", phil.position, phil.cycles);
 }
 
