7bd824eea9763cd2e3a581efb2d71cd18eba3e8e
ShantMElikyan <shantmelikyan@gmail.com>
2020-05-11 17:18:26 -0700

    some progress, but not there yet

--- lab6/mole.c
+++ lab6/mole.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
 
-int main ()
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
 {
-    printf("hello world");
+    static FILE* file = NULL;
+  
+    file = fopen("/lab6.log", "a");
+    if (file == NULL) {
+        perror("Error opening file: ");
+    }
+    else{
+        fprintf(file, "Pop %s\n", argv[0]);
+        fclose(file);
+    }
+    
+    return 0;
+    
 }
+
+
--- lab6/spiritd.c
+++ lab6/spiritd.c
@@ -7,27 +7,25 @@
 #include <sys/resource.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <string.h>
+#include <time.h>
 
-static int exit_flag = 0;
-
-void signal_handler(int signal);
-/*
-void new_mole()
-{
+char PATH[256];
+static pid_t mole = 0;
 
 
-}
-*/
+static void signal_handler(int signal);
+void create_mole_process();
 
 int main(/*int argc, char **argv*/)
 {
-    
     pid_t pid;
-    umask(0);
-    pid = fork();
+    pid_t session; // daemon
     struct rlimit rlimit;
-    int dev_null;
+    int dev_null; // file desctiptor
+    int change_check; 
 
+    pid = fork();
     if(pid == -1){
         printf("Can't create a process.");
         exit(EXIT_FAILURE);
@@ -35,42 +33,56 @@ int main(/*int argc, char **argv*/)
     /* child */
     if(pid == 0){
         printf("Child working\n");
-        if(signal(SIGTERM, signal_handler) == SIG_ERR )
-            fprintf(stderr, "Can't handle signal");
-        pid_t session;
-        session = setsid();
         printf("PID: %d\n", getpid() );
+        mole = 0;
+        umask(0);
+
+        session = setsid();
         if(session == -1){
-            printf("Can't create a new session.");
-            exit(EXIT_FAILURE);
-        }
-        else
-            printf("session created\n");
-
-        int change_check; 
-        change_check = chdir("/");
-
-        if(change_check == -1){
-            printf("Can't changed directory!");
+            fprintf(stderr, "Can't create a new session.");
             exit(EXIT_FAILURE);
         }
         
+        change_check = chdir("/");
+        if(change_check == -1){
+            fprintf(stderr, "Can't changed directory!");
+            exit(EXIT_FAILURE);
+        }
+       
         getrlimit(RLIMIT_NOFILE, &rlimit);
         if (rlimit.rlim_max == RLIM_INFINITY)
             rlimit.rlim_max = 1024;
         for (unsigned int i = 0; i < rlimit.rlim_max; i++)
             close(i);
-        dev_null=open("/dev/null", O_RDONLY);
-        dup2(dev_null,0);
-        dup2(dev_null,1);
-        dup2(dev_null,2);
+      
+        dev_null=open("/dev/null", O_WRONLY);
+        dup2(dev_null, STDIN_FILENO);
+        dup2(dev_null, STDOUT_FILENO);
+        dup2(dev_null, STDERR_FILENO);
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+        
+        
+        if(signal(SIGTERM, signal_handler) == SIG_ERR ){
+                fprintf(stderr, "Can't handle signal");
+        }
+        if(signal(SIGUSR1, signal_handler) == SIG_ERR ){
+                fprintf(stderr, "Can't handle signal");
+        }
+        if(signal(SIGUSR2, signal_handler) == SIG_ERR ){
+                fprintf(stderr, "Can't handle signal");
+                
+        }
+        
         do{
             sleep(1);
-        }while (exit_flag == 0);
+        }while(1);
+
     }
     /* parent */
     else{
-        printf("Termintaing parent\n");
+        fprintf(stderr, "Termintaing parent\n");
         exit(EXIT_SUCCESS);
     }
 
@@ -78,13 +90,60 @@ int main(/*int argc, char **argv*/)
 
 }
 
-void signal_handler(int sig)
+static void signal_handler(int sig)
 {
-    signal(SIGTERM, signal_handler);
+    signal(sig, signal_handler);
+    
     if (sig == SIGTERM){
-        exit_flag = 1;
+        if(mole != 0)
+        {
+            kill(mole, SIGKILL);  
+        }
+        kill(getpid(), SIGKILL);
+        exit(EXIT_SUCCESS);
     }
+    else if(sig == SIGUSR1 || sig == SIGUSR2){
+        if(mole != 0)
+            kill(mole, SIGKILL);
 
-    fprintf(stderr, "signal catched!\n Killing daemon!\n");
+        create_mole_process();
+    }
+    else
+    {
+        fprintf(stderr,"unknnown sig\n");
+    }
+    
+}
+
+ 
+void create_mole_process()
+{
+    char* newArgv[3];
+    char* mole_num_str = NULL;
+    char PATH[1024];
+    pid_t new_mole;
+    /* setup new argv to pass to mole */
+   
+    new_mole = fork();
+    if(new_mole == 0){
+
+        srand(time(NULL));
+        int rand_num = (rand() & 1);
+        if(rand_num == 0)
+            sprintf(mole_num_str, "%s", "mole1");
+        else
+            sprintf(mole_num_str, "%s", "mole2");
+        newArgv[0] = mole_num_str;
+        getcwd(PATH, 1024);
+        newArgv[1] = PATH; 
+        strcat(PATH, "/mole");
+        newArgv[2] = NULL;
+
+        if(execv(PATH, newArgv) == -1){
+            perror ("failed to locate mole executable.\n");
+        }
+      
+    }
+    
 
 }
