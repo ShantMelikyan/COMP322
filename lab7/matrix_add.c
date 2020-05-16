169687358f57a97439f925389a28f0d017477f73
shantmelikyan <shantmelikyan@gmail.com>
2020-05-16 15:26:16 -0700

    seting up the project

---
+++ lab7/matrix_add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+
+    printf("hello from Matrix_add\n");
+
+    return 0;
+}
---
+++ lab7/matrix_gen.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    printf("hello from Matrix_gen\n");
+
+    return 0;
+}
