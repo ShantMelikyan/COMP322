f67445f2abf1de0c484d8f734ecdc5b66ed2cbc2
shantmelikyan <shantmelikyan@gmail.com>
2020-05-17 01:50:29 -0700

    setup continued

--- lab7/matrix_add.c
+++ lab7/matrix_add.c
@@ -1,10 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/times.h>
+#include <time.h>
+#include <limits.h>
+#include <aio.h>
+
+void matrix_add();
 
 int main(int argc, char** argv)
 {
+    if (argc != 3){
+        fprintf(stderr, "Inccorrect input. Try again!\n");
+    }
+    time_t start_time, end_time;
+    srand(time(0)); 
+    int matrix_size = atoi(argv[1]);
+    int blocks = atoi(argv[2]);
+    int block_size = matrix_size / atoi(argv[2]);
+    const int BUF_SIZE = block_size * block_size * 4;
+    int scalar = (rand() % (100 - (-100) + 1)) + (-100);
 
-    printf("hello from Matrix_add\n");
+    if(matrix_size % blocks){
+        fprintf(stderr, "WRONG PARAMETERS OF SIZE OR BLOCK");
+        exit(EXIT_FAILURE);
+    }
+
+    time(&start_time);
+    fprintf(stderr,"START: %d\n", (int)start);
+    fprintf(stderr,"BUF_SIZE: %d\n", BUF_SIZE);
+    fprintf(stderr,"MATRIX_SIZE: %d\n", matrix_size);
+    fprintf(stderr,"BLOCK_SIZE: %d\n", block_size);
+    fprintf(stderr, "SCALAR: %d\n", scalar);
+
+    for(i = 0; i < blocks*blocks-1; i++)
+    {
+        // for y = 1 .. blocks
+        //     async read request matrix[x, y]
+        //     block = async read return matrix[x, y]
+        //     matrix_add(block, block_size, scaler)
+        //     async write request block
+        //     async write return block
+
+    }
+    time(&end_time);
+    fprintf(stderr,"STOP:  %ld\n", start);
 
     return 0;
 }
+
+
+void matrix_add()
+{
+
+    
+
+}
+
+
--- lab7/matrix_gen.c
+++ lab7/matrix_gen.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> 
+
 
 int main(int argc, char** argv)
 {
-    printf("hello from Matrix_gen\n");
+
+    if (argc != 2) {
+        fprintf(stderr, "Inccorrect input. Try again!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int matrix_size = atoi(argv[1]);
+    srand(time(0));
+    
+    /* generate random matrix size*size */ 
+    for(int i = 0; i < matrix_size; i++){
+        for( int j = 0; j < matrix_size; j++)
+            printf("%d ", (rand() % (100 - (-100) + 1)) + (-100));
+        printf("\n");
+    }
+
 
     return 0;
 }
